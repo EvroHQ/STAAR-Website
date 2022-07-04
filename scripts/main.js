10f1186a306119a18580b18838442899d239523a
EvroHQ <kirkoussis.christophe@hotmail.com>
2022-07-05 01:00:04 +0200

    update guide img

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
 // STAAR JS //
 
+// Mobile Menu Disable Scroll //
 document.querySelector('.toggler').addEventListener('click', function () { document.querySelector('body').classList.toggle('no-scroll') });
+
+
+// Smooth Scrolling //
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
